fb1c8dcafcc6a7e2509bda2987258c1e5636b604
augustotsilva <augustotsilvaa@gmail.com>
2025-11-29 16:10:20 -0300

    added basic starting endpoints (no DB yet)

--- src/main/java/com/prologapp/api/controller/VehicleController.java
+++ src/main/java/com/prologapp/api/controller/VehicleController.java
@@ -1,8 +1,9 @@
 package com.prologapp.api.controller;
 
+import com.prologapp.api.service.VehicleService;
 import com.prologapp.domain.Vehicle;
-import com.prologapp.domain.enums.VehicleBrandEnum;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +13,19 @@ import java.util.List;
 @RequestMapping("/v1/vehicle")
 public class VehicleController {
 
+    private final VehicleService vehicleService;
+
+    public VehicleController(VehicleService vehicleService) {
+        this.vehicleService = vehicleService;
+    }
+
     @GetMapping("/list")
-    public List<Vehicle> listVehicles() {
-        return List.of(new Vehicle(1, "DBZ8001", VehicleBrandEnum.SCANIA, 1200.1, true, List.of()));
+    public List<Vehicle> getAllVehicles() {
+        return vehicleService.getAllVehicles();
+    }
+
+    @GetMapping("/{id}")
+    public Vehicle getVehicleById(@PathVariable Integer id) {
+        return vehicleService.getVehicleById(id);
     }
 }
